5f87cf248831b557eb7b4f5fa20d0d44d8c36f6f
A&ECoding <l.kieran95@gmail.com>
2025-02-11 23:07:14 +0100

    Update export_pt_to_onnx_opset.py

--- export_pt_to_onnx_opset.py
+++ export_pt_to_onnx_opset.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Charger ton modèle YOLOv8 entraîné
-model = YOLO("best_fortv2.pt")  # Mets ici le bon chemin vers ton modèle
+model = YOLO("best.pt")  # Mets ici le bon chemin vers ton modèle
 
 # Exporter en ONNX avec opset 12 (supporté par OpenCV)
 model.export(format="onnx", opset=12)
